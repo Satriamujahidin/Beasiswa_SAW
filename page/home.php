1ffb6b3218a31c3b8620194a96423be63de46f9d
Satriamujahidin <124132738+Satriamujahidin@users.noreply.github.com>
2023-12-10 18:26:49 +0700

    Add files via upload

---
+++ page/home.php
@@ -0,0 +1,10 @@
+<body bgcolor="Pink">
+<h1><p align="center">
+<font color="blue">
+   	Selamat Datang di Sistem Pendukung Keputusan <br>
+	Penentuan Penerima Beasiswa di STMIK Adhi Guna <br>
+	Menggunakan Metode Simple Additive Weighting (SAW)
+</h1> 
+</b>
+</p>
+</font>
